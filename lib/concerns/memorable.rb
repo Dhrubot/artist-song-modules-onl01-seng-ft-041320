a00bd47f98b9912de74b26225d47b7c1cbb9c374
Dhrubo Talukder <dhrubo.dh@gmail.com>
2020-05-01 07:29:43 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,5 +1,6 @@
 module Memorable
   module ClassMethods
+    
     def reset_all
       all.clear
     end
@@ -7,4 +8,6 @@ module Memorable
     def count
       all.count
     end
+    
+  end
 end
